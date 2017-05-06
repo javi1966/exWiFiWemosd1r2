067e4f5c0ece9c270ed8060979b106e7183effe4
javi1966 <JEG1966@telefonica.net>
2017-05-06 10:40:34 +0200

    cambios
    cambios

--- exWiFiWemosd1r2.ino
+++ exWiFiWemosd1r2.ino
@@ -1,6 +1,15 @@
 #include <ESP8266WiFi.h>
+#include <SPI.h>
+#include "MCP3002.h"
+
+#define  LED  5  //D1 es GPIO5
+
+
+MCP3002 adc(SS);
+
 
 WiFiServer server(80);
+WiFiClient client;
 
 //char buf[100];
 String buf ;
@@ -10,16 +19,31 @@ String estadoI = "OK";
 float fValorU = 225.6;
 String estadoU = "NOK";
 
+bool bActualiza = true;
+const int timerUpdate = 1; //1/2 minuto
+String writeAPIKey = "F81DR9CCLURUGK87";
+const char* host = "api.thingspeak.com";
+int valor;
+float amp;
+float tension = 224;
+
 void setup() {
   // put your setup code here, to run once:
+
+  pinMode(A0, INPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(115200);
+  adc.begin();
 
   delay(100);
+
+  digitalWrite(LED, HIGH);
+
 
   Serial.println("\r\n");
   Serial.print("Chip ID: 0x");
   Serial.println(ESP.getChipId(), HEX);
-  
+
   WiFi.mode(WIFI_STA);
   //WiFi.softAP("MOVISTAR_47E8","ndfBakCEvtHwj8jSSEMJ");
   // WiFi.softAP("Wireless-N","z123456z");
@@ -61,6 +85,40 @@ void setup() {
     Serial.println(WiFi.softAPIP());
   }
 
+  
+  valor = adc.analogRead(0);
+  Serial.println(String(valor));
+  amp = (15.5 * (valor - 512) / 355); // 355 valor adc 867 - valor vcc/2 512
+
+
+  digitalWrite(LED, HIGH); //flashing led
+  delay(500);
+  digitalWrite(LED, LOW);
+
+  if (client.connect(host, 80)) {
+
+    // Construct API request body
+    String body = "field1=";
+    body +=  String(amp);
+    body += "&field2=";
+    body += String(tension);
+
+    Serial.println(body);
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + writeAPIKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(body.length());
+    client.print("\n\n");
+    client.print(body);
+    client.print("\n\n");
+
+  }
+
+  client.stop();
 
   IPAddress http_server_ip = WiFi.localIP();
 
@@ -69,10 +127,68 @@ void setup() {
   Serial.print("nuestra server IP:");
   Serial.print(http_server_ip);
   Serial.print("\r\n");
+
+  noInterrupts();
+  timer0_isr_init();
+  timer0_attachInterrupt(TimingISR);
+  timer0_write(ESP.getCycleCount() + 80000000L); // 80MHz == 1sec
+  interrupts();
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  char temp[10];
+  
+
+  if (bActualiza) {
+
+    valor = adc.analogRead(0);
+    Serial.println(String(valor));
+    amp = (15.5 * (valor - 508) / 233); // 355 valor adc 867 - valor vcc/2 512
+    /*
+      sprintf(temp, "%2f", amp);
+      Serial.println(String(V));
+
+    */
+    if (client.connect(host, 80)) {
+
+      // Construct API request body
+      String body = "field1=";
+      body +=  String(amp);
+      body += "&field2=";
+      body += String(tension);
+
+
+
+
+      Serial.println(body);
+
+      client.print("POST /update HTTP/1.1\n");
+      client.print("Host: api.thingspeak.com\n");
+      client.print("Connection: close\n");
+      client.print("X-THINGSPEAKAPIKEY: " + writeAPIKey + "\n");
+      client.print("Content-Type: application/x-www-form-urlencoded\n");
+      client.print("Content-Length: ");
+      client.print(body.length());
+      client.print("\n\n");
+      client.print(body);
+      client.print("\n\n");
+
+    }
+
+    client.stop();
+
+    digitalWrite(LED, HIGH); //flashing led
+    delay(500);
+    digitalWrite(LED, LOW);
+
+    bActualiza = false;
+
+  }
+
+
   WiFiClient client = server.available();
 
   if (client) {
@@ -84,9 +200,6 @@ void loop() {
       Serial.print(req);
       Serial.print("\r\n");
 
-
-
-
       if (req.indexOf("MonitorEnergia") > 0)
       {
         /*client.println("HTTP/1.1 200 OK");
@@ -96,9 +209,9 @@ void loop() {
         if (req.indexOf("corriente") > 0) {
 
           buf += "{\"Corriente\":{\"Valor\":";
-          buf += "\"";
-          buf += String(fValorI);
-          buf += "\"";
+
+          buf += String(amp);
+
           buf += ",\"status\":";
           buf += "\"";
           buf += String(estadoI);
@@ -110,9 +223,9 @@ void loop() {
         else if (req.indexOf("voltaj") > 0) {
 
           buf += "{\"Tension\":{\"Valor\":";
-          buf += "\"";
-          buf += String(fValorU);
-          buf += "\"";
+
+          buf += String(tension);
+
           buf += ",\"status\":";
           buf += "\"";
           buf += String(estadoU);
@@ -140,8 +253,22 @@ void loop() {
 
   }
 
+}
 
 
+//****************************************************************************************************
+void TimingISR() {
 
+  static int cntTemp = 0;
+
+  if (++cntTemp > timerUpdate) // 1 minuto
+  {
+    bActualiza = true;
+
+    cntTemp = 0;
+  }
+  //bActualiza = true;
+
+  timer0_write(ESP.getCycleCount() + 80000000L); // 80MHz == 1sec
 
 }
